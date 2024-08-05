ff70fa7d60251ad385a9eacb92f8762c58322dab
A Holcsik <you@example.com>
2024-08-05 17:28:26 +0200

    vittula located on the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import places from "../../utils/places.json";
 
+console.log(places);
 function Map() {
   return (
     <div>
       <MapContainer
         style={{ height: "750px" }}
-        center={[51.505, -0.09]}
+        center={[47.497913, 19.040236]}
         zoom={13}
         scrollWheelZoom={false}
       >
@@ -14,6 +16,13 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {places.map((marker) => {
+          return (
+            <Marker position={[marker.location.lat, marker.location.lng]}>
+              <Popup>{marker.name}</Popup>
+            </Marker>
+          );
+        })}
         <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
