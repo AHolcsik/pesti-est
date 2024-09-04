031494e8bd192c8473afbbc55916c9c27757eb65
A Holcsik <you@example.com>
2024-09-04 16:28:45 +0200

    corrected linting

--- src/app/api/add-pet/route.ts
+++ src/app/api/add-pet/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: Request) {
   try {
     if (!petName || !ownerName) throw new Error("Pet and owner names required");
     await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
-  } catch (error) {
+  } catch (error: unknown) {
     return NextResponse.json({ error }, { status: 500 });
   }
 
